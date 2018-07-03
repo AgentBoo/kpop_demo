8d06b78795c9f9bfa7e8af3da5f7d4783e6ffdbc
agentboo <zuzi.han@gmail.com>
2018-07-03 14:55:28 -0400

    added settings

--- census/settings.py
+++ census/settings.py
@@ -60,6 +60,18 @@ else:
         'default': dj_database_url.config(conn_max_age=600)  
     }
 
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+
+    SECURE_BROWSER_XSS_FILTER = True 
+
+    SECURE_SSL_REDIRECT = True
+
+    SESSION_COOKIE_SECURE = True 
+
+    CSRF_COOKIE_SECURE = True 
+
+    X_FRAME_OPTIONS='DENY' 
+
 
 # Application definition
 
